d44e712252fcc70858b44a40d9ab55bf8938539b
Muhammad Ishfaq <m.ishfaq@otif-solutions.com>
2022-06-29 11:46:35 +0500

    minor changes in traits

--- src/Traits/ShortUrlTrait.php
+++ src/Traits/ShortUrlTrait.php
@@ -9,18 +9,18 @@ trait ShortUrlTrait {
         $details = Tracker::all();
         if ($details){
 
-            return response()->json(['success' => ['details' => $details]]);
+            return response()->json(['message'=>'success' , 'data' => $data]);
         }
-        return response()->json(['error' => ['data not found']]);
+        return response()->json(['errors'=>['error' => ['data not found']]],403);
     }
 
     public static function getData($ipAddress) {
 
         $data = Tracker::where('ip_address', $ipAddress)->get();
         if ($data) {
-            return response()->json(['success' => ['data' => $data]]);
+            return response()->json(['message'=>'success' , 'data' => $details]);
         }
-        return response()->json(['error' => ['data not found']]);
 
+        return response()->json(['errors'=>['error' => ['data not found']]],403);
     }
 }
